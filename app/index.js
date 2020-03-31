cd7e0f074b7a86fd86c2f060e4fe704ae46227aa
Madeline Streilein <mcstreilein@gmail.com>
2020-03-31 18:48:41 -0400

    Initial react/webpack/tailwind config

---
+++ app/index.js
@@ -0,0 +1,11 @@
+import React, { useEffect, useState } from 'react'
+import ReactDOM from 'react-dom'
+import './index.scss'
+
+function App() {
+  return (
+    <div>Hello</div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('app'))
