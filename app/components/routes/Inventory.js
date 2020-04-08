7bf03ed0723ed8ac6d43f7284843cd642b35fdbc
Madeline Streilein <mcstreilein@gmail.com>
2020-04-08 14:54:20 -0400

    Subscribe to item stream and display on inventory page

--- app/components/routes/Inventory.js
+++ app/components/routes/Inventory.js
@@ -1,7 +1,40 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import * as FirestoreService from '../../firestore'
 
 function Inventory() {
-  return <h1>Inventory Page</h1>
+  // temporarily set groupId for dev.
+
+  const [groupId, setGroupId] = useState(
+    'zAGGtLgp4nmnKVGdPeiw'
+  )
+  const [items, setItems] = useState()
+
+  // Use an effect hook to subscribe to the item stream and
+  // automatically unsubscribe when the component unmounts.
+  useEffect(() => {
+    const unsubscribe = FirestoreService.streamItems(
+      groupId,
+      {
+        next: (querySnapshot) => {
+          const updatedItems = querySnapshot.docs.map(
+            (docSnapshot) => docSnapshot.data()
+          )
+          setItems(updatedItems)
+        }
+      }
+    )
+    return unsubscribe
+  }, [groupId, setItems])
+
+  return (
+    <React.Fragment>
+      <h1>Inventory Page</h1>
+      {items &&
+        items.map((item, i) => (
+          <div key={i}>{item.name}</div>
+        ))}
+    </React.Fragment>
+  )
 }
 
 export default Inventory
