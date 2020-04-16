a7be184fba42d4b9735ee5363c57eadf10b90d9d
Madeline Streilein <mcstreilein@gmail.com>
2020-04-16 17:22:41 -0400

    Move react-dates component to item detail screen

--- app/components/routes/Inventory.js
+++ app/components/routes/Inventory.js
@@ -3,14 +3,6 @@ import AddItemToGroup from '../forms/AddItemToGroup'
 import ItemsList from './ItemsList'
 import { bool, array, object } from 'prop-types'
 
-// React-Date.
-import { DayPickerRangeController } from 'react-dates'
-import ThemedStyleSheet from 'react-with-styles/lib/ThemedStyleSheet'
-import aphroditeInterface from 'react-with-styles-interface-aphrodite'
-import DefaultTheme from 'react-dates/lib/theme/DefaultTheme'
-ThemedStyleSheet.registerInterface(aphroditeInterface)
-ThemedStyleSheet.registerTheme(DefaultTheme)
-
 // Firebase/firestore.
 import * as FirestoreService from '../../firestore'
 
@@ -21,11 +13,6 @@ function Inventory(props) {
 
   // Form.
   const [isAddFormVisible, setIsAddFormVisible] = useState(false);
-
-  // Date picker.
-  const [startDate, setStartDate] = useState(null)
-  const [endDate, setEndDate] = useState(null)
-  const [focusedInput, setFocusedInput] = useState('startDate')
 
   useEffect(() => {
     if (groupIds.length > 0) {
@@ -58,25 +45,13 @@ function Inventory(props) {
   return (
     <main>
       <h1>Inventory Page</h1>
-      <div className='container flex flex-row justify-evenly'>
+      <div className='flex flex-row justify-evenly'>
         <section className='flex flex-col'>
           <h2>Your Inventory</h2>
           {items.length > 0
           ? (<ItemsList items={items} {...props} />)
           : (<div>Loading...</div>)
           }
-        </section>
-        <section className='flex'>
-          <DayPickerRangeController
-            startDate={startDate}
-            endDate={endDate}
-            onDatesChange={({ startDate, endDate }) => {
-              setStartDate(startDate)
-              setEndDate(endDate)
-            }}
-            focusedInput={focusedInput}
-            onFocusChange={(focusedInput) => setFocusedInput(focusedInput)}
-          />
         </section>
         <section className='flex flex-col'>
           {groups && (
--- app/components/routes/ItemDetail.js
+++ app/components/routes/ItemDetail.js
@@ -1,9 +1,32 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+
+// React-Date.
+import { DayPickerRangeController } from 'react-dates'
+import ThemedStyleSheet from 'react-with-styles/lib/ThemedStyleSheet'
+import aphroditeInterface from 'react-with-styles-interface-aphrodite'
+import DefaultTheme from 'react-dates/lib/theme/DefaultTheme'
+ThemedStyleSheet.registerInterface(aphroditeInterface)
+ThemedStyleSheet.registerTheme(DefaultTheme)
 
 function ItemDetail(props) {
+  // Date picker.
+  const [startDate, setStartDate] = useState(null)
+  const [endDate, setEndDate] = useState(null)
+  const [focusedInput, setFocusedInput] = useState('startDate')
+
   return (
     <main>
       <h1>Item #{props.match.params.itemId}</h1>
+      <DayPickerRangeController
+        startDate={startDate}
+        endDate={endDate}
+        onDatesChange={({ startDate, endDate }) => {
+          setStartDate(startDate)
+          setEndDate(endDate)
+        }}
+        focusedInput={focusedInput}
+        onFocusChange={(focusedInput) => setFocusedInput(focusedInput)}
+      />
     </main>
   )
 }
