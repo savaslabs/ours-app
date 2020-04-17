628c495596e75e9a8c7086c1a4dacc9398a8dcb7
Madeline Streilein <mcstreilein@gmail.com>
2020-04-17 15:15:07 -0400

    Ensure detail pages aren't nav links

--- app/ProtectedRoutes.js
+++ app/ProtectedRoutes.js
@@ -11,35 +11,40 @@ const protectedRoutes = [
     exact: true,
     path: '/inventory',
     main: (props) => <Inventory {...props} />,
-    public: false
+    public: false,
+    inNav: true
   },
   {
     name: 'Community',
     exact: true,
     path: '/community',
     main: (props) => <Community {...props} />,
-    public: false
+    public: false,
+    inNav: true
   },
   {
     name: 'Profile',
     exact: true,
     path: '/me',
     main: (props) => <Profile {...props} />,
-    public: false
+    public: false,
+    inNav: true
   },
   {
     name: 'ItemDetail',
     exact: true,
     path: '/inventory/:itemId',
     main: (props) => <ItemDetail {...props} />,
-    public: false
+    public: false,
+    inNav: false
   },
   {
     name: 'GroupDetail',
     exact: true,
     path: '/community/:groupId',
     main: (props) => <GroupDetail {...props} />,
-    public: false
+    public: false,
+    inNav: false
   }
 ]
 
--- app/components/Header.js
+++ app/components/Header.js
@@ -11,10 +11,11 @@ const Header = () => {
         <ul className='flex flex-row justify-between'>
           {isLoggedIn &&
             protectedRoutes.map((route, i) => (
-              route.name === 'ItemDetail' ? null :
-              <li key={i} className='flex'>
-                <Link to={route.path}>{route.name}</Link>
-              </li>
+              route.inNav
+              ? <li key={i} className='flex'>
+                  <Link to={route.path}>{route.name}</Link>
+                </li>
+              : null
             ))}
         </ul>
       </nav>
